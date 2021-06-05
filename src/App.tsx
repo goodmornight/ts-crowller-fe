e0377284844ea861d6773c68283bec1f431058e3
GoodNight <goodnightlilanhui@gmail.com>
2021-06-05 23:09:15 +0800

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,17 @@
-import { Form, Input, Button, Checkbox } from 'antd';
-import './login.css';
+import React from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import LoginPage from './Pages/Login';
 
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
-};
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 },
-};
-
-const Login = () => {
-  const onFinish = (values: any) => {
-    console.log('Success:', values);
-  };
-
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-  };
-
+function App() {
   return (
-    <div className="login-page">
-      <Form
-        {...layout}
-        name="basic"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item {...tailLayout}>
-          <Button type="primary" htmlType="submit">
-            登录
-          </Button>
-        </Form.Item>
-      </Form>
+    <div>
+      <HashRouter>
+        <Switch>
+          <Route path="/login" exact component={LoginPage} />
+        </Switch>
+      </HashRouter>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from './App';
+import App from './App';
 import 'antd/dist/antd.css';
 
-ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
