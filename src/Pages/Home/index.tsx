fe5af97421316ad20aaedb023e77ba9d66230622
GoodNight <goodnightlilanhui@gmail.com>
2021-06-06 21:34:05 +0800

    faeat: add echarts

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Button, message } from 'antd';
+import echarts from 'echarts';
+import ReactEcharts from 'echarts-for-react';
 import axios from 'axios';
+import moment from 'moment';
 import './style.css';
 
+interface CourseItem {
+  title: string;
+  count: number;
+}
+
+interface LineData {
+  name: string;
+  type: string;
+  data: number[];
+}
+
+interface State {
+  loaded: boolean;
+  isLogin: boolean;
+  data: {
+    [key: string]: CourseItem[]
+  }
+}
+
 class Home extends Component {
-  state = {
+  state: State = {
     loaded: false,
-    isLogin: true
+    isLogin: true,
+    data: {}
   };
 
   componentDidMount() {
@@ -23,7 +46,26 @@ class Home extends Component {
         });
       }
     });
+
+    axios.get('/api/showData').then(res => {
+      if (res.data) {
+        this.setState({
+          data: {...this.state.data, ...res.data}
+        })
+      }
+    });
   }
+
+  handleCrowllerClick = () => {
+    axios.get('/api/getData').then(res => {
+      if (res.data?.data) {
+        message.success('爬取成功');
+      } else {
+        message.error('爬取失败');
+      }
+    });
+  }
+
   handleLogoutClick = () => {
     axios.get('/api/logout').then(res => {
       if (res.data?.data) {
@@ -36,17 +78,79 @@ class Home extends Component {
     });
   }
 
+  getOption = () => {
+    const { data } = this.state;
+    const courseNames: string[] = [];
+    const times: string[] = [];
+    const tempData: {
+      [key: string]: number[];
+    } = {};
+    for(let i in data) {
+      const item = data[i];
+      times.push(moment(Number(i)).format('MM-DD HH:mm'));
+      item.forEach(inner => {
+        const { title, count } = inner;
+        if (courseNames.indexOf(title) === -1) {
+          courseNames.push(title);
+        }
+        tempData[title] ? tempData[title].push(count) : (tempData[title] = [count]);
+      })
+    }
+    console.log(times)
+    const result: LineData[] = [];
+    for (let i in tempData) {
+      result.push({
+        name: i,
+        type: 'line',
+        data: tempData[i]
+      });
+    }
+    return {
+      title: {
+        text: '课程在线学习人数'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: courseNames
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: times
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: result
+    };
+  }
+
   render() {
     const { isLogin, loaded } = this.state;
     if (isLogin) {
       if (loaded) {
         return (
           <div className="home-page">
-            <Button type="primary" style={{ marginLeft: '5px' }}>
-              爬取
-            </Button>
-            <Button type="primary">展示</Button>
-            <Button type="primary" onClick={this.handleLogoutClick}>退出</Button>
+            <div className="buttons">
+              <Button type="primary" style={{ marginRight: '10px' }}  onClick={this.handleCrowllerClick}>
+                爬取
+              </Button>
+              <Button type="primary" onClick={this.handleLogoutClick}>退出</Button>
+            </div>
+            <ReactEcharts option={this.getOption()} />
           </div>
         );
       }
